f0495cca5e23c6a8475966a5e26f81014c62bfe0
Niv Suresh <nivsuresh06@gmail.com>
2025-11-01 00:00:38 +0000

    Creating and adding RQ Provider

--- app/providers/ReactQueryProvider.tsx
+++ app/providers/ReactQueryProvider.tsx
@@ -1,7 +1,14 @@
-"use client"
+"use client";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import {ReactNode, useState} from 'react'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactNode, useState } from "react";
 
+export default function ReactQueryProvider({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  const [client] = useState(() => new QueryClient());
 
-
+  return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
+}
